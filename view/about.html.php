eafcef61dc99ce0edf368bc63f055281e9963918
Alkior <maxelfman@yandex.ru>
2020-02-04 16:08:40 +0400

    4 commit

--- view/about.html.php
+++ view/about.html.php
@@ -2,7 +2,14 @@
     <h2><b>О нас</b></h2><br>
     <ul>Данный блог разработан в ходе обучения особенностям языка PHP, и не служит примером "отменного" кода, но уже способен показать понимание основ ООП и разработки в целом.
         А далее пойдет вода: Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam fugiat quaerat, quo, optio, neque aut minus unde praesentium iste suscipit voluptatum? Ea itaque, suscipit ipsa et quo mollitia dolor consequatur!
-</ul><br><br>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad consequuntur error voluptas magni hic tenetur delectus, maxime doloremque qui nihil sed dolorum nisi at impedit est optio consectetur dolores labore!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim iste illo ipsum quaerat, doloremque cum deserunt, perferendis assumenda amet vitae nisi nam ullam itaque voluptatem minus, praesentium soluta nobis! Numquam.
+</ul>
+<ul>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam excepturi hic fuga ipsa nostrum, soluta voluptatibus dolorum fugiat dolores illo? Sed perspiciatis sit ducimus tenetur mollitia atque repellat laborum aspernatur?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, nobis atque quasi dolor vel vero veritatis sunt officiis? Consectetur nobis nam optio dolor assumenda rerum aliquam eum repellat dolores? Maiores.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, nam dolorum inventore provident perferendis cupiditate ipsam quam asperiores? Obcaecati est, eius voluptate perferendis excepturi quo cumque sed quisquam perspiciatis laboriosam!
+</ul>
+<br><br>
 
 </div>
 <div class="container mx-lg-2">
